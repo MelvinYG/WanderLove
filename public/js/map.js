e693165a1ebc726cadb8afbdfe3f06b79172b117
Melvin YG <melvingeorge204@gmail.com>
2024-01-25 21:14:00 +0530

    Map update

--- public/js/map.js
+++ public/js/map.js
@@ -4,15 +4,17 @@
 // ADD YOUR ACCESS TOKEN FROM
 // https://account.mapbox.com
 mapboxgl.accessToken = mapToken;
+let lng = listing.geometry.coordinates[0];
+let lt = listing.geometry.coordinates[1];
 const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: "mapbox://styles/mapbox/streets-v12",
-  center: listing.geometry.cordinates, // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  center: [lng,lt], // starting position [lng, lat]
+  zoom: 10 // starting zoom
 });
 
-// // console.log(listing.geometry);
-// const marker1 = new mapboxgl.Marker()
-//   .setLngLat(listing.geometry.cordinates)
-//   .setPopup(new mapboxgl.Popup({offset : 25}).setHTML("<h5>Hello World</h5>"))
-//   .addTo(map);
+// console.log(listing.geometry);
+const marker1 = new mapboxgl.Marker({color: 'red'})
+  .setLngLat([lng,lt])
+  .setPopup(new mapboxgl.Popup({offset : 25}).setHTML("<h5>Hello World</h5>"))
+  .addTo(map);
